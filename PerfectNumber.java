288cf2971aa4e28f044d2d4a3c4c15ad82820fb2
amankhanp <34658950+amankhanp@users.noreply.github.com>
2017-12-20 00:19:28 +0530

    Perfect number Program

---
+++ PerfectNumber.java
@@ -0,0 +1,18 @@
+package etoday;
+
+public class PerfectNumber {
+	public static void main(String args[]){
+
+		for(int dividend=1;dividend<10001;dividend++){
+			int sum=0;
+			for(int divisor =1;divisor<dividend;divisor++){
+				if(dividend%divisor==0){
+					sum = sum + divisor;
+				}
+			}
+			if(sum == dividend){
+				System.out.println("It is Perfect Number : "+sum);
+			}
+		}
+	}
+}
