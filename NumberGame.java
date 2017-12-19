9ddf57db20a5ba7c45b5396d4c0584bceb9a2f03
amankhanp <34658950+amankhanp@users.noreply.github.com>
2017-12-19 22:45:36 +0530

    Number Game
    Printing from 1 to 100 numbers. if the number can divided by 3 print "Fizz" instead of the number and if the number can divided by 5 then print "Buzz", if the number can satisfies both the condition then print "Boom".

---
+++ NumberGame.java
@@ -0,0 +1,19 @@
+package etoday;
+public class NumberGame {
+	public static void main(String args[]){
+		for(int number=1;number<101;number++){
+			if(number%3==0 && number%5==0){
+				System.out.println("Boom");
+			}
+			else if(number%3==0){
+				System.out.println("Fizz");
+			}
+			else if(number%5==0){
+				System.out.println("Buzz");
+			}
+			else{
+				System.out.println(number);
+			}
+		}
+	}
+}
