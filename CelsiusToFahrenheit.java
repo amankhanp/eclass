bee3dc08a16424bc81ad206fdc9f1e3595489f1d
Aman Khan <34658950+amankhanp@users.noreply.github.com>
2017-12-20 01:25:52 +0530

    Update CelsiusToFahrenheit.java

--- CelsiusToFahrenheit.java
+++ CelsiusToFahrenheit.java
@@ -4,15 +4,15 @@ public class CelsiusToFahrenheit {
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
 		Double celsius,fahrenheit=0.0;
-		System.out.println("Enter 0 to convert celsius-fahrenheit and 1 to convert fahrenheit-celsius : ");
+		/* System.out.println("Enter 0 to convert celsius-fahrenheit and 1 to convert fahrenheit-celsius : ");
 		int enter= sc.nextInt();
 		switch(enter){
-		case 0 :
+		case 0 : */
 			System.out.print("Enter Celsius Number : ");
 			celsius = sc.nextDouble();
 			fahrenheit = (celsius * 9/5.0) + 32;
 			System.out.println("Temperature in Fahrenhiet : "+fahrenheit);
-			break;
+		/*	break;
 		case 1 :
 			System.out.print("Enter Fahrenhiet Number : ");
 			fahrenheit = sc.nextDouble();
@@ -21,6 +21,6 @@ public class CelsiusToFahrenheit {
 			break;
 		default :
 			System.out.println("Invalid option");
-		}
+		} */
 	}
 }
